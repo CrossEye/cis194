5be9afcda27b4aff447d69995301041e49afeb75
Scott Sauyet <scott@sauyet.com>
2014-12-02 14:04:43 -0500

    Finishing credit-card problem

--- Week01/creditCard.hs
+++ Week01/creditCard.hs
@@ -1,12 +1,36 @@
-toDigitsRev:: Integer -> [Integer]
-toDigitsRev n
-  | n <= 0    = []
-  | otherwise = n `mod` 10 : toDigitsRev ((n - (n `mod` 10)) `div` 10)
-
-rev:: [Integer] -> [Integer] -- obviously could be more generic
-rev xs = rev' xs []  where
-  rev' (x:xs) acc = rev' xs (x:acc)
-  rev' []     acc = acc
-
 toDigits:: Integer -> [Integer]
-toDigits n = rev (toDigitsRev n)
+toDigits 0 = []
+toDigits n
+  | n > 0     = (toDigits ((n - (n `mod` 10)) `div` 10)) ++ [n `mod` 10]
+  | otherwise = []
+
+rev:: [a] -> [a]
+rev [] = []
+rev (x:xs) = (rev xs) ++ [x]
+
+toDigitsRev:: Integer -> [Integer]
+toDigitsRev n = rev (toDigits n)
+
+flatten:: [[a]] -> [a]
+flatten [] = []
+flatten (x:y) = x ++ (flatten y)
+
+doubleOddIndices:: [Integer] -> [Integer]
+doubleOddIndices [] = []
+doubleOddIndices (x:[]) = [x]
+doubleOddIndices (x:(y:z)) = x : ((2 * y) : doubleOddIndices(z))
+
+doubleEveryOther:: [Integer] -> [Integer]
+doubleEveryOther ls = rev (doubleOddIndices (rev ls))
+
+allDigits:: [Integer] -> [Integer]
+allDigits x = flatten (map toDigits x)
+
+sumDigits:: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = x + sumDigits(xs)
+
+validate:: Integer -> Bool
+validate n 
+  | sumDigits (allDigits (doubleEveryOther (toDigits n))) `mod` 10 == 0 = True
+  | otherwise = False
